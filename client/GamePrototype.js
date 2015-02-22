00b914f8e2c4d55ac667cc55dc0c78b49c992e88
Adam <adam.niedzialkowski@gmail.com>
2015-02-22 17:34:38 +0100

    oj ;/

--- client/GamePrototype.js
+++ client/GamePrototype.js
@@ -5,21 +5,9 @@ GamePrototype.prototype.init = function (){
 GamePrototype.prototype.onStart = function (){
   throw new Exception("onStart not implemented!");
 };
-GamePrototype.prototype.finish = function(){
-  throw new Exception("Finish not implemented!");
-};
-GamePrototype.prototype.onConnectionClose = function (){
-  throw new Exception("onConnectionClose not implemented!");
-};
-GamePrototype.prototype.disconnect = function(){
-  throw new Exception("Disconnect not implemented!");
-};
-GamePrototype.prototype.onRoomUpdate = function(){
-  throw new Exception("OnRoomUpdate not implemented");
-}
 GamePrototype.prototype.dataHandler = function(){
   throw new Exception("dataHandler not implemented");
-}
+};
 
 function clone (obj) {
   if (!obj) return;
