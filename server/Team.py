b55bec37553c5a685633197b14f07319c2c86978
Dominik Rosiek <45dominik@gmail.com>
2015-03-12 16:21:46 +0100

    refresh

--- server/Team.py
+++ server/Team.py
@@ -3,23 +3,23 @@
 import socket
 
 if __name__ == "__main__":
-	raise Exception("You cannot run this file directly")
+    raise Exception("You cannot run this file directly")
 
 class Team(object):
-	def __init__(self):
-		self.players = []
-		self.id = id(self)
+    def __init__(self):
+        self.players = []
+        self.id = id(self)
         self.name = "test name"
-	
-	def addPlayer(self, player):
-		self.players.append(player)
-		player.team = self
-		return self
-	
-	def removePlayer(self, player):
-		try:
-			index = self.players.index(player)
-			player.team = None
-			del self.players[index]
-		except KeyError:
-			return True
+    
+    def addPlayer(self, player):
+        self.players.append(player)
+        player.team = self
+        return self
+    
+    def removePlayer(self, player):
+        try:
+            index = self.players.index(player)
+            player.team = None
+            del self.players[index]
+        except KeyError:
+            return True
